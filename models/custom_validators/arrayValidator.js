02668475f37bcf1c3650359aa754fd4ebad57d02
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-17 20:53:19 -0500

    Add validator for checking if length is within two values

--- models/custom_validators/arrayValidator.js
+++ models/custom_validators/arrayValidator.js
@@ -6,3 +6,13 @@ exports.lengthEquals = (length) => {
     `Array.length must be equal to ${length}`,
   ];
 };
+
+exports.lengthWithin = (minLength, maxLength) => {
+  return [
+    (val) => {
+      if (val.length >= minLength && val.length <= maxLength) return true;
+      return false;
+    },
+    `Array.length must be >= ${minLength} and <= ${maxLength}.`,
+  ];
+};
