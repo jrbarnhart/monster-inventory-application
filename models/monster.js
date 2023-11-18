dcf984d28a5a185bc0d2821df5acf32c58dcee47
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-17 20:31:17 -0500

    Add and export monster schema

---
+++ models/monster.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const isIntValidator = require("./custom_validators/isIntValidator");
+const arrayValidator = require("./custom_validators/arrayValidator");
+
+const MonsterSchema = new mongoose.Schema({
+  name: { type: String, minLength: 2, maxLength: 20, required: true },
+  family: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Family",
+    required: true,
+  },
+  info: { type: String, maxLength: 200, required: true },
+  stock: { type: Number, min: 0, required: true, validate: isIntValidator },
+  innate_abilities: {
+    type: [mongoose.Schema.Types.ObjectId],
+    validate: arrayValidator.lengthEquals(3),
+  },
+});
+
+module.exports = mongoose.model("Monster", MonsterSchema);
