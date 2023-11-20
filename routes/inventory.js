912527fed6c2dd6fdce64bf7786c2743b6b8bc96
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-19 20:55:09 -0500

    Update route

--- routes/inventory.js
+++ routes/inventory.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const family_controller = require("../controllers/familyController");
 
 /* GET home page. */
-router.get("/", family_controller.family_list);
+router.get("/inventory", family_controller.family_list);
 
 module.exports = router;
