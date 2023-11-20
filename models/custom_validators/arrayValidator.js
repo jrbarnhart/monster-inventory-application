cd6baea703d5ef7da98912642b450556d3982b1d
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-19 19:37:33 -0500

    Make sure params are ints or return false

--- models/custom_validators/arrayValidator.js
+++ models/custom_validators/arrayValidator.js
@@ -1,18 +1,22 @@
 exports.lengthEquals = (length) => {
   return [
     (val) => {
+      if (!Number.isInteger(length)) length = -1;
       return val.length === length;
     },
-    `Array.length must be equal to ${length}`,
+    `Array.length must be equal to length param (must be int).`,
   ];
 };
 
 exports.lengthWithin = (minLength, maxLength) => {
   return [
     (val) => {
+      if (!Number.isInteger(minLength) || !Number.isInteger(maxLength))
+        return false;
+      if (minLength > maxLength) return false;
       if (val.length >= minLength && val.length <= maxLength) return true;
       return false;
     },
-    `Array.length must be >= ${minLength} and <= ${maxLength}.`,
+    `Array.length must be equal to or within min/maxLength params (must be ints).`,
   ];
 };
