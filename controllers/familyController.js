fc0a185416fc63bf718d798f1cdc9270ba5a8c11
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-19 20:08:05 -0500

    Add family controller skeleton

---
+++ controllers/familyController.js
@@ -0,0 +1,22 @@
+const Family = require("../models/family");
+
+const asyncHandler = require("express-async-handler");
+
+// List all families
+exports.family_list = asyncHandler(async (req, res, next) => {
+  res.send("NYI: List families GET");
+});
+
+// Display details for specific family
+
+// Display create family form on GET
+
+// Handle create family form on POST
+
+// Display delete family form on GET
+
+// Handle delete family form on POST
+
+// Display update family form on GET
+
+// Handle update family form on POST
