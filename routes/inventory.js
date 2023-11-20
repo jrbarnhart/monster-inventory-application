2099737e6c564fbddc75ea64b8225fdfbb2d0c96
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-19 20:59:18 -0500

    Replace var with const

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const family_controller = require("../controllers/familyController");
 
