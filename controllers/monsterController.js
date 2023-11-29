c302cb05abc64ad94f47882e721644df9b8a8d6c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-29 18:46:37 -0500

    Add method for handling create monster post

--- controllers/monsterController.js
+++ controllers/monsterController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 const Monster = require("../models/monster");
 const Family = require("../models/family");
@@ -62,9 +63,66 @@ exports.monster_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle create monster form on POST
-exports.monster_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NYI: Create monster POST");
-});
+exports.monster_create_post = [
+  body("name")
+    .trim()
+    .isLength({ min: 2, max: 20 })
+    .escape()
+    .withMessage("Name must be between 2 and 20 characters."),
+  body("family").escape(),
+  body("info")
+    .trim()
+    .isLength({ max: 200 })
+    .escape()
+    .withMessage("Info must be 200 characters for less."),
+  body("skill-1", "skill-2", "skill-3").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const errorsArray = errors.array();
+
+    const selectedSkills = [
+      req.body.skill_0,
+      req.body.skill_1,
+      req.body.skill_2,
+    ];
+    const skillsAreUnique = selectedSkills.every((value) => {
+      return (
+        selectedSkills.indexOf(value) === selectedSkills.lastIndexOf(value)
+      );
+    });
+
+    if (!skillsAreUnique) {
+      errorsArray.push({ msg: "Skills must be unique." });
+    }
+
+    const monster = new Monster({
+      name: req.body.name,
+      family: req.body.family,
+      info: req.body.info,
+      innate_skills: selectedSkills,
+    });
+
+    if (!errors.isEmpty() || !skillsAreUnique) {
+      const [allFamilies, allSkills] = await Promise.all([
+        Family.find({}).sort({ name: 1 }).exec(),
+        Skill.find({}).sort({ name: 1 }).exec(),
+      ]);
+
+      console.log(monster);
+
+      res.render("monster_create", {
+        title: "Create a Monster",
+        family_list: allFamilies,
+        skill_list: allSkills,
+        monster: monster,
+      });
+    } else {
+      await monster.save();
+      res.redirect(monster.url);
+    }
+  }),
+];
 
 // Display delete monster form on GET
 exports.monster_delete_get = asyncHandler(async (req, res, next) => {
