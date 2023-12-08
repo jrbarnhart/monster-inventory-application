328491d0743b10643072345205095686d13c50e2
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-12-08 15:02:20 -0500

    Add method for update post request

--- controllers/monsterInstanceController.js
+++ controllers/monsterInstanceController.js
@@ -50,7 +50,7 @@ exports.monsterinstance_create_get = asyncHandler(async (req, res, next) => {
 exports.monsterinstance_create_post = [
   body("nickname")
     .trim()
-    .isLength({ min: 1, max: 5 })
+    .isLength({ min: 1, max: 20 })
     .escape()
     .withMessage("Nickname must be between 1 and 5 characters."),
   body("monster").escape(),
@@ -162,7 +162,7 @@ exports.monsterinstance_update_get = asyncHandler(async (req, res, next) => {
   }
 
   res.render("monsterinstance_create", {
-    title: "Create a Monster Instance",
+    title: "Update Monster Instance",
     monsterinstance: monsterInstance,
     monster_list: allMonsters,
     skill_list: allSkills,
@@ -170,6 +170,97 @@ exports.monsterinstance_update_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle update monsterinstance form on POST
-exports.monsterinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NYI: update monsterinstance POST");
-});
+exports.monsterinstance_update_post = [
+  body("nickname")
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .escape()
+    .withMessage("Nickname must be between 1 and 5 characters."),
+  body("monster").escape(),
+  body("gender").trim().toLowerCase().escape(),
+  body("level")
+    .trim()
+    .isInt({ min: 1, max: 99 })
+    .escape()
+    .withMessage("Level must be an int between 1-99"),
+  body("health", "magic", "attack", "defense", "agility", "intelligence")
+    .trim()
+    .isInt({ min: 0, max: 999 })
+    .escape()
+    .withMessage("Stats must be an int between 0-999."),
+  body(
+    "skill_0",
+    "skill_1",
+    "skill_2",
+    "skill_3",
+    "skill_4",
+    "skill_5",
+    "skill_6",
+    "skill_7"
+  ).escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const errorsArray = errors.array();
+
+    const selectedSkills = [
+      req.body.skill_0,
+      req.body.skill_1,
+      req.body.skill_2,
+      req.body.skill_3,
+      req.body.skill_4,
+      req.body.skill_5,
+      req.body.skill_6,
+      req.body.skill_7,
+    ].filter((value) => value !== "");
+
+    console.log(selectedSkills);
+
+    const skillsAreUnique = selectedSkills.every((value) => {
+      return (
+        selectedSkills.indexOf(value) === selectedSkills.lastIndexOf(value)
+      );
+    });
+
+    if (!skillsAreUnique) {
+      errorsArray.push({ msg: "Skills must be unique." });
+    }
+
+    const monsterinstance = new MonsterInstance({
+      monster: req.body.monster,
+      nickname: req.body.nickname,
+      level: req.body.level,
+      health: req.body.health,
+      magic: req.body.magic,
+      attack: req.body.attack,
+      defense: req.body.defense,
+      agility: req.body.agility,
+      intelligence: req.body.intelligence,
+      gender: req.body.gender,
+      skills: selectedSkills,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty || !skillsAreUnique) {
+      const [allMonsters, allSkills] = await Promise.all([
+        Monster.find({}).sort({ name: 1 }).exec(),
+        Skill.find({}).sort({ name: 1 }).exec(),
+      ]);
+
+      res.render("monsterinstance_create", {
+        title: "Update Monster Instance",
+        monster_list: allMonsters,
+        skill_list: allSkills,
+        monsterinstance: monsterinstance,
+        errors: errorsArray,
+      });
+    } else {
+      const updatedMonsterInstance = await MonsterInstance.findByIdAndUpdate(
+        req.params.id,
+        monsterinstance,
+        {}
+      );
+      res.redirect(updatedMonsterInstance.url);
+    }
+  }),
+];
